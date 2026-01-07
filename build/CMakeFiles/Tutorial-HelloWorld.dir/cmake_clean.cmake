20c193a14976b6e7c3ea60793dd5ecf23f10a0a6
Moon <coeus.ti@outlook.com>
2026-01-07 16:21:09 -0600

    Built with CMake

---
+++ build/CMakeFiles/Tutorial-HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/moon/Documents/projects/IPFLow/Tutorial-HelloWorld"
+  "/home/moon/Documents/projects/IPFLow/Tutorial-HelloWorld.pdb"
+  "CMakeFiles/Tutorial-HelloWorld.dir/src/network_flow/main.cpp.o"
+  "CMakeFiles/Tutorial-HelloWorld.dir/src/network_flow/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial-HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
