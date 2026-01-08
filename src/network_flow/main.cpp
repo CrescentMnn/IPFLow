4fa777880b0113a6da39f8ea491c6fb6fa9d92d5
Moon <coeus.ti@outlook.com>
2026-01-08 08:05:30 -0600

    Created base file | READ AND FILTER PACKETS

--- src/network_flow/main.cpp
+++ src/network_flow/main.cpp
@@ -4,29 +4,50 @@
 #include <PcapFileDevice.h>
 
 int main(){
+   
+    std::unique_ptr<pcpp::IFileReaderDevice> reader(pcpp::IFileReaderDevice::getReader("test.pcap"));
+    if(reader == nullptr){
+        std::cerr << "Error determining the pcap file" << std::endl;
+	return 1;
+    }
+
+    if(!reader->open()){
+        std::cerr << "Error opening file..." << std::endl;
+	return 1;
+    }
+
+    // will be written to it
+    pcpp::PcapFileWriterDevice pcapWriter("output.pcap", pcpp::LINKTYPE_ETHERNET);
     
-    pcpp::PcapFileReaderDevice reader("test.pcap");
-    if(!reader.open()){
-        std::cerr << "Error opening the pcap file" << std::endl;
+    if(!pcapWriter.open()){
+        std::cerr << "Cannot open file for writing.." << std::endl;
+	return 1;
+    }
+
+    if(!reader->setFilter("net 192.168.1.114")){
+        std::cerr << "Error applying filter..." << std::endl;
 	return 1;
     }
 
     pcpp::RawPacket rawPacket;
-    if(!reader.getNextPacket(rawPacket)){
-        std::cerr << "Couldnt read the first packet in file" << std::endl;
-	return 1;
+    //if(!reader.getNextPacket(rawPacket)){
+    //    std::cerr << "Couldnt read the first packet in file" << std::endl;
+    //	return 1;
+    //}
+
+    while(reader->getNextPacket(rawPacket)){
+        pcapWriter.writePacket(rawPacket);
+        pcpp::Packet parsedPacket(&rawPacket);
+
+        if(parsedPacket.isPacketOfType(pcpp::IPv4)){
+            pcpp::IPv4Address srcIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIPv4Address();
+	    pcpp::IPv4Address dstIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
+
+	    std::cout << "Source IP is: " << srcIP << "\nDestination IP is: " << dstIP << std::endl;
+        }
     }
 
-    pcpp::Packet parsedPacket(&rawPacket);
-
-    if(parsedPacket.isPacketOfType(pcpp::IPv4)){
-        pcpp::IPv4Address srcIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIPv4Address();
-	pcpp::IPv4Address dstIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
-
-	std::cout << "Source IP is: " << srcIP << "\nDestination IP is: " << dstIP << std::endl;
-    }
-
-    reader.close();
+    reader->close();
 
     return 0;
 }
